f2ba03bd0312c86ff98966694fdefaab5303dcfe
Andrew Cushing <andrew.cushing@kearsarge.org>
2023-10-25 08:20:58 -0400

    Setup

---
+++ Getters.py
@@ -0,0 +1,100 @@
+def getMenuOption(debug = False):
+    if debug: print("getMenuOption Function")
+    
+    goodInput = False
+    
+    while not goodInput:
+        option = input("Please select an option: ")
+        option = option.lower()
+        if debug: print(option)
+        if (option == "q" or 
+            option == "quit" or
+            option == "x" or
+            option == "exit"):
+                option = "q"
+                goodInput = True
+            
+        elif (option == "1" or 
+            option == "one" or
+            option == "story 1" or
+            option == "story1"):
+                option = "1"
+                goodInput = True
+            
+        else:
+            print("Please make a valid choice")
+        
+    return option
+
+def getWord(prompt, debug = False):
+    if debug: print("getMenuOption Function")
+    
+    goodInput = False
+    
+    while not goodInput:
+        word = input(prompt)
+        goodInput = True
+        if isSwear(word, debug):
+            goodInput = False
+            print ("Don't use language like that")
+            
+    return word
+
+def getSport(prompt, debug = False):
+    if debug: print("getSport Function")
+    
+    goodInput = False
+    
+    sports = ["soccer", 
+    "football", 
+    "basketball", 
+    "hockey", 
+    "golf"]
+    
+    while not goodInput:
+        word = input(prompt)
+        goodInput = True
+        if isSwear(word, debug):
+            goodInput = False
+            print ("Don't use language like that")
+        elif word.lower() not in sports:
+            goodInput = False
+            print("Sorry, I don't know that one")
+            
+            
+    return word
+
+def isSwear(word, debug = False):
+    if debug: print("isSwear Fucntion")
+    if word.lower() in swearList:
+        return True
+    else:
+        return False
+
+     
+swearList = ["poop",
+"pee",
+"shit",
+"piss",
+"pee",
+"dumbass",
+"kike",
+"ass",
+"fuck",
+"nigga",
+"nigger",
+"bitch",
+"fag",
+"gay",
+"cock",
+"dick",
+"penis",
+"retard",
+"moron",
+"idiot",
+"pussy",
+"wanker",
+"cunt",
+"clit",
+"bastard",
+"whore" ]
---
+++ Story1.py
@@ -0,0 +1,14 @@
+from Getters import*
+
+def Story1(debug = False):
+    if debug: print("Story1 Function")
+
+    print("\n")
+    friendName1 = getWord("Enter a name: ", debug)
+    sport1 = getSport("Enter a sport: ", debug)
+
+    out = "\n"
+    out += "One day me and my friend, " + friendName1
+    out+= " were out playing " + sport1
+    
+    return out
