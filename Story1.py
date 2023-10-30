82f207fc616c1e7925c6d7e449db9af16a458b23
austin Needham <austin.needham@kearsarge.org>
2023-10-30 08:24:36 -0400

    work on story1

--- Story1.py
+++ Story1.py
@@ -1,4 +1,4 @@
-Rfrom Getters import*
+from Getters import*
 
 def Story1(debug = False):
     if debug: print("Story1 Function")
@@ -6,10 +6,14 @@ def Story1(debug = False):
     print("\n")
     friendName1 = getWord("Enter a name: ", debug)
     sport1 = getSport("Enter a sport: ", debug)
+    adj1 = getAdj(" Enter a adj:" , debug)
 
     out = "\n"
-    out += "One day me and my friend, " + friendName1
-    out+= " were out playing " + sport1
+    out += "One day me and my friend" + friendName1
+    out += " were out playing " + sport1
+    out += "and he was" + adj1
+    
+
     
     return out
 
