23bb81735dedf6e0fc683f43ab2c04ef1f6caa57
Andrew Cushing <andrew.cushing@kearsarge.org>
2023-11-01 08:07:45 -0400

    story1

--- Story1.py
+++ Story1.py
@@ -6,7 +6,7 @@ def Story1(debug = False):
     print("\n")
     friendName1 = getWord("Enter a name: ", debug)
     sport1 = getSport("Enter a sport: ", debug)
-    adj1 = getAdj(" Enter a adj:" , debug)
+    adj1 = getAdj("Enter a adjective:" , debug)
     place1 = getPlace(" Enter a place:", debug)
     activity1 =getActivity(" Enter a activity:", debug)
     animal1 = getAnimal (" Enter a animal:", debug)
@@ -17,7 +17,7 @@ def Story1(debug = False):
     out += " were out playing " + sport1
     out += " and he was " + adj1
     out += ". After, we went to the " + place1
-    out += " and we went " + activity1
+    out += " and we were " + activity1
     out += ". While this was going on my friend " + friendName1 
     out += " noticed a giant " + animal1
     out += " and it " + action1
