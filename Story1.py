83d191565ea3d4b02296c4962c210bd98c11900c
Andrew Cushing <andrew.cushing@kearsarge.org>
2023-11-01 09:00:32 -0400

    story1

--- Story1.py
+++ Story1.py
@@ -7,9 +7,9 @@ def Story1(debug = False):
     friendName1 = getWord("Enter a name: ", debug)
     sport1 = getSport("Enter a sport: ", debug)
     adj1 = getAdj("Enter a adjective:" , debug)
-    place1 = getPlace(" Enter a place:", debug)
-    activity1 =getActivity(" Enter a activity:", debug)
-    animal1 = getAnimal (" Enter a animal:", debug)
+    place1 = getPlace("Enter a place:", debug)
+    activity1 =getActivity("Enter a activity:", debug)
+    animal1 = getAnimal ("Enter a animal:", debug)
     action1 = getAction ("Enter a action word: ", debug)
 
     out = "\n"
