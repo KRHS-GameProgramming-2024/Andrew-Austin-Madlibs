68f31e7a49e357c3f36bc4db543c2f0322650cb9
austin Needham <austin.needham@kearsarge.org>
2023-11-06 08:05:02 -0500

    kjhkhku

---
+++ Story3.py
@@ -0,0 +1,17 @@
+from Getters import*
+
+def Story3(debug = False):
+    if debug: print("Story3 Function")
+
+    print("\n")
+    friendName1 = getWord("Enter a name: ", debug)
+    sport1 = getSport("Enter a sport: ", debug)
+    adj1 = getAdj("Enter a adjective:" , debug)
+    place1 = getPlace("Enter a place:", debug)
+    activity1 =getActivity("Enter a activity:", debug)
+    animal1 = getAnimal ("Enter a animal:", debug)
+    action1 = getAction ("Enter a action word: ", debug)
+
+    out = "\n"
+
+    return out
