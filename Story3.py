c0f6700be15c5bd35800e8925cb0f0453a83df5e
Andrew Cushing <andrew.cushing@kearsarge.org>
2023-11-08 08:08:01 -0500

    story3

--- Story3.py
+++ Story3.py
@@ -4,16 +4,8 @@ def Story3(debug = False):
     if debug: print("Story3 Function")
 
     print("\n")
-    friendName1 = getWord("Enter a name: ", debug)
-    sport1 = getSport("Enter a sport: ", debug)
-    adj1 = getAdj("Enter a adjective:" , debug)
-    place1 = getPlace("Enter a place:", debug)
-    activity1 =getActivity("Enter a activity:", debug)
-    animal1 = getAnimal ("Enter a animal:", debug)
-    action1 = getAction ("Enter a action word: ", debug)
-
-    out = "\n"
     
+    out += "\n"
     out += "One day me and my friend " + friendName1
     out += " were out playing " + sport1
     out += " and he was " + adj1
